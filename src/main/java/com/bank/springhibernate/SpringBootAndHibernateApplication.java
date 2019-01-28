300fbeb62adf0757d5dde2b35e43054c1342b49e
Jenwit Penjamrat <jenwit_penjamrat@hotmail.com>
2019-01-28 12:03:42 +0700

    Initial

---
+++ src/main/java/com/bank/springhibernate/SpringBootAndHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.bank.springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndHibernateApplication.class, args);
+	}
+
+}
+
