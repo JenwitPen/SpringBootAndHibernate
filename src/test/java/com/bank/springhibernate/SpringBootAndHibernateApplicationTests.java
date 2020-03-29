31bc969e38e9b6e7ccb7cdd17c61616b1a06579e
Jenwit Penjamrat <jenwit.penjamrat@scb.co.th>
2020-03-29 14:06:18 +0700

    test

--- src/test/java/com/bank/springhibernate/SpringBootAndHibernateApplicationTests.java
+++ src/test/java/com/bank/springhibernate/SpringBootAndHibernateApplicationTests.java
@@ -1,9 +1,13 @@
 package com.bank.springhibernate;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,7 +15,13 @@ public class SpringBootAndHibernateApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		// then
+		assertEquals(3,3);
 	}
-
+	@Test
+	public void contextLoads2() {
+		// then
+		assertEquals(3,3);
+	}
 }
 
